44959ba48f6d7ca3882ea9f56c7529c2a8156ede
Karla Lucia Böttcher <karla.boettcher@posteo.de>
2025-11-29 17:17:39 +0100

    fixed an oopsie

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import { store } from "./state/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    // the App component is wrapped in a redux store provider, so that the entire
-    // application has access to the global store.
+    {/* // the App component is wrapped in a redux store provider, so that the entire
+    // application has access to the global store. */}
     <Provider store={store}>
       <App />
     </Provider>
